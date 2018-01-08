1cd54a0e15ef852b38abeb8aaf5452a8a5de9de1
Altarrel <31830955+Altarrel@users.noreply.github.com>
2018-01-08 18:31:45 -0500

    Error handling for missing args

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -13,6 +13,8 @@ class ErrorHandler:
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
             return
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f"You must provide {error.param.name}.")
         else:
             print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
             traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
