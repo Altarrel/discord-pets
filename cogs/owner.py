bb7c524b77f9b25c9440d175ddfe30ff9652a85c
Altarrel <31830955+Altarrel@users.noreply.github.com>
2018-01-13 18:28:53 -0500

    copied eval from MystBot

--- cogs/owner.py
+++ cogs/owner.py
@@ -5,6 +5,7 @@ import traceback
 
 def setup(bot):
     bot.add_cog(Owner(bot))
+    bot.add_cog(Eval(bot))
 
 class Owner:
     """Commands only for the bot owner"""
@@ -117,3 +118,77 @@ class Owner:
         await self.bot.db.release(connection)
 
         await ctx.send(f"{user} was unblocked.")
+
+class Eval:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    def cleanup_code(self, content):
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+        return content.strip('` \n')
+
+    def get_syntax_error(self, e):
+        if e.text is None:
+            return f'```py\n{e.__class__.__name__}: {e}\n```'
+        return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
+
+    @commands.command(name='eval', hidden=True)
+    @commands.is_owner()
+    async def _eval(self, ctx, *, body: str):
+
+        env = {
+            'bot': ctx.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': ctx.bot._last_result,
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        code = textwrap.indent(body, '  ')
+        to_compile = f'async def func():\n{code}'
+
+        try:
+            exec(to_compile, env)
+        except SyntaxError as e:
+            return await ctx.send(self.get_syntax_error(e))
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('🔘')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    if len(value) > 2000:
+                        gist = await self.bot.create_gist('Eval', [(f'eval.py', value)])
+                        await ctx.send(gist)
+                    else:
+                        await ctx.send(f'```py\n{value}\n```')
+            else:
+                ctx.bot._last_result = ret
+                fmt = f'{value}{ret}'
+
+                if len(fmt) > 1989:  # len('```py\n\n```') == 10, one extra char for good measure
+                    code = textwrap.dedent(fmt).replace('`', '\uFEFF')
+                    gist = await self.bot.create_gist('Eval', [('eval.py', f'{code}')])
+                    await ctx.send(f'**Eval was uploaded as a gist.**\n {gist}')
+                else:
+                    await ctx.send(f'```py\n{fmt}\n```')
