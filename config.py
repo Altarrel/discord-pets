9dd244318d76c2f5f1e7f335573dc32f3c1126d8
Unknown <31830955+Altarrel@users.noreply.github.com>
2018-01-16 16:56:17 -0500

    properly ignore config.py pls

--- config.py
+++
@@ -1,4 +0,0 @@
-BOT_TOKEN = "APP TOKEN"
-DBL_TOKEN = "DISOCRD BOTS LIST TOKEN"
-prefix = "dp!"
-credentials = {"user": "USERNAME", "password": "PASSWORD", "database": "DATABASE", "host": "127.0.0.1"}
