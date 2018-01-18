3fc907af2ce030aae7a3af39c01dcc184b1fba7f
Unknown <31830955+Altarrel@users.noreply.github.com>
2018-01-17 21:23:27 -0500

    added dice roll minigame

--- game_data/minigames.py
+++ game_data/minigames.py
@@ -1,61 +1,68 @@
-import discord
 import asyncio
 import random
 
+import discord
+
 async def coinflip(bot, ctx):
-    msg = await ctx.send(f"{ctx.author} | Guess whether the coin will land on heads or tails.")
-    use_msg = False
-    winnings = None
-    if ctx.channel.permissions_for(ctx.author).add_reactions:
-        try:
-            await msg.add_reaction("\U0001f1ed")
-            await msg.add_reaction("\U0001f1f9")
-        except discord.Forbidden:
-            await msg.edit(content=f"{ctx.author} | Guess whether the coin will land on heads or tails.\n\n**I don't have add_reactions permissions, say h/heads or t/tails.**")
-            use_msg = True
-    else:
-        use_msg = True
+    await ctx.send(f"{ctx.author} | Guess whether the coin will land on heads or tails.")
+    def check(m):
+        return m.author == ctx.author and m.channel == ctx.channel and m.content.lower() in ("h", "heads", "t", "tails")
+    try:
+        response = await bot.wait_for("message", check=check, timeout=20)
+    except asyncio.TimeoutError:
+        await ctx.send(f"{ctx.author} | \U000023f0 You took too long to respond, you lose.")
+        return
 
-    if use_msg:
-        await msg.edit(content=f"{ctx.author} | Guess whether the coin will land on heads or tails.\n\n**I or you don't have add_reactions permissions, say h/heads or t/tails.**")
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel and m.content.lower() in ("h", "heads", "t", "tails")
-        try:
-            guess = await bot.wait_for("message", check=check, timeout=20)
-        except asyncio.TimeoutError:
-            await ctx.send(f"{ctx.author} | You took too long to respond, you lose.")
-            return
+    flip = random.choice((("h", "heads"), ("t", "tails")))
+    if not response.content.lower() in flip:
+        await ctx.send(f"{ctx.author} | \U0001f626 You guessed incorrectly! The coin landed on {flip[1]}.")
+        return
 
-        answer = random.choice((("h", "heads"), ("t", "tails")))
-        if guess.content.lower() in answer:
-            winnings = random.randint(10, 25)
-            await ctx.send(f"{ctx.author} | You won {winnings} pet coins! The coin landed on {answer[1]}. \U0001f389")
-        else:
-            await ctx.send(f"{ctx.author} | You didn't guess correctly! The coin landed on {answer[1]}. \U0001f626")
-    else:
-        def check(reaction, user):
-            return user == ctx.author and str(reaction.emoji) in ("\U0001f1ed", "\U0001f1f9")
-        try:
-            guess = await bot.wait_for("reaction_add", check=check, timeout=20)
-        except asyncio.TimeoutError:
-            await ctx.send(f"{ctx.author} | You took too long to respond, you lose.")
-            return
+    winnings = random.randint(10, 15)
+    await ctx.send(f"{ctx.author} | \U0001f389 You guessed correctly! The coin landed on {flip[1]}. You won {winnings} coins.")
+    connection = await bot.db.acquire()
+    async with connection.transaction():
+        query = """UPDATE users SET currency = currency + $1 WHERE id = $2"""
+        await connection.execute(query, winnings, ctx.author.id)
+    await bot.db.release(connection)
 
-        answer = random.choice((("\U0001f1ed", "heads"), ("\U0001f1f9", "tails")))
-        if str(guess[0].emoji) == answer[0]:
-            winnings = random.randint(10, 25)
-            await ctx.send(f"{ctx.author} | You won {winnings} pet coins! The coin landed on {answer[1]}. \U0001f389")
-        else:
-            await ctx.send(f"{ctx.author} | You didn't guess correctly! The coin landed on {answer[1]}. \U0001f626")
 
-    if winnings:
-        connection = await bot.db.acquire()
-        async with connection.transaction():
-            query = """UPDATE users SET currency = currency + $1 WHERE id = $2;"""
-            await connection.execute(query, winnings, ctx.author.id)
-        await bot.db.release(connection)
 
-# async def gamble(bot, ctx):
+
+# async def pick_the_toy(bot, ctx):
+#     msg = await ctx.send(f"{ctx.author} | Choose a toy for your pet to play with.\n1. {}\n2. {}\n3. {}")
+#     winnings = None
+#     if not ctx.channel.permissions_for(ctx.author).add_reactions or not ctx.channel.permissions_for(bot.user).add_reactions:
+#         await msg.edit(content=f"{ctx.author} | Choose a toy for your pet to play with.\n\n**Reactions failed, please send your answer. 1, 2, or 3?**")
+#         use_msg = True
+
+#     if use_msg:
+
+
+
+async def dice(bot, ctx):
+    await ctx.send(f"{ctx.author} | \U0001f3b2 Where do you think the die will land? Pick a number 1-6.")
+    def check(m):
+        return m.author == ctx.author and m.channel == ctx.channel and m.content in ("1", "2", "3", "4", "5", "6")
+    try:
+        response = await bot.wait_for("message", check=check, timeout=20)
+    except asyncio.TimeoutError:
+        await ctx.send(f"{ctx.author} | \U000023f0 You took too long to respond, you lose.")
+        return
+
+    roll = random.randint(1, 6)
+    if roll != int(response.content):
+        await ctx.send(f"{ctx.author} | \U0001f626 You guessed incorrectly! The die landed on {roll}.")
+        return
+
+    winnings = random.randint(15, 30)
+    await ctx.send(f"{ctx.author} | \U0001f389 You guessed correctly! The die landed on {roll}. You won {winnings} coins.")
+    connection = await bot.db.acquire()
+    async with connection.transaction():
+        query = """UPDATE users SET currency = currency + $1 WHERE id = $2;"""
+        await connection.execute(query, winnings, ctx.author.id)
+    await bot.db.release(connection)
+
+
     
-
-all_minigames = (coinflip,)
+all_minigames = (coinflip, dice)
