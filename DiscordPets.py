77dced51acbfb13a80b6b8eb2be6f65ce0a4bccf
Unknown <31830955+Altarrel@users.noreply.github.com>
2018-02-07 15:20:44 -0500

    moved guild count posting to background task

--- DiscordPets.py
+++ DiscordPets.py
@@ -46,7 +46,10 @@ class DiscordPets(commands.Bot):
         self.blocked = kwargs.pop("blocked")
         self.loop.create_task(self.load_all_extensions())
 
+        self.loop.create_task(self.update_guild_count())
+
     async def on_message(self, message):
+        return
         await self.wait_until_ready()
         await asyncio.sleep(1)
         # Stop certain users and bots from using the bot
@@ -61,35 +64,22 @@ class DiscordPets(commands.Bot):
         print(f"Username: {self.user.name}\n"
               f"ID: {self.user.id}\n")
 
-        if self.user.id in config.TEST_IDS:
-            return
+    async def update_guild_count(self):
+        await self.wait_until_ready()
+        await asyncio.sleep(1)
+        while not self.is_closed():
+            if self.user.id in config.TEST_IDS:
+                print("Logged in under a TEST_ID, not posting guild count to discord bot list.")
+                return
 
-        headers = {'Authorization': config.DBL_TOKEN}
-        data = {'server_count': len(self.guilds)}
-        api_url = 'https://discordbots.org/api/bots/' + str(self.user.id) + '/stats'
-        async with aiohttp.ClientSession() as session:
-            await session.post(api_url, data=data, headers=headers)
-        print("Guild count posted to https://discordbots.org/")
+            headers = {'Authorization': config.DBL_TOKEN}
+            data = {'server_count': len(self.guilds)}
+            api_url = 'https://discordbots.org/api/bots/' + str(self.user.id) + '/stats'
+            async with aiohttp.ClientSession() as session:
+                await session.post(api_url, data=data, headers=headers)
+            print("Posted guild count to discord bot list")
 
-    async def on_guild_join(self, guild):
-        if self.user.id in config.TEST_IDS:
-            return
-
-        headers = {'Authorization': config.DBL_TOKEN}
-        data = {'server_count': len(self.guilds)}
-        api_url = 'https://discordbots.org/api/bots/' + str(self.user.id) + '/stats'
-        async with aiohttp.ClientSession() as session:
-            await session.post(api_url, data=data, headers=headers)
-
-    async def on_guild_remove(self, guild):
-        if self.user.id in config.TEST_IDS:
-            return
-
-        headers = {'Authorization': config.DBL_TOKEN}
-        data = {'server_count': len(self.guilds)}
-        api_url = 'https://discordbots.org/api/bots/' + str(self.user.id) + '/stats'
-        async with aiohttp.ClientSession() as session:
-            await session.post(api_url, data=data, headers=headers)
+            await asyncio.sleep(600)
 
     async def load_all_extensions(self):
         await self.wait_until_ready()
